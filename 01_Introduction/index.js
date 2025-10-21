a9995236bab454fbba015d5485e9cb6c5024fe1d
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-21 11:27:15 +0530

    WORKING ON INTRODUECTION IN NODE JS

--- 01_Introduction/index.js
+++ 01_Introduction/index.js
@@ -1,1 +1,13 @@
-const http= reqire('http://google.com/');
+const http= require('http')
+const server = http.createServer((req,res) =>{
+    if (req.url === '/about') {
+        res.end('About Page');
+    } else if (req.url === '/contact'){
+        res.end('contact Page')
+    }
+    else{
+        res.writeHead(404)
+        res.end('Page Not Found')
+    }
+})
+server.listen(3000)
