bfb8d8fc4957fb8544341bcc853706dbe97f7ce8
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-29 11:57:08 +0530

    STARTED WITH STARTING THE PROJECT NODE WORKSPACE

--- 01-node-demo/app.js
+++ 01-node-demo/app.js
@@ -1,9 +1,23 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+// const http = require('http')
+
+// let server = http.createServer();
+
+// server.listen(3200,()=>{
+//     console.log('Application is running in port number 3200');
+    
+// })
+
 
 const http = require('http')
 
-let server = http.createServer();
-
+let server = http.createServer((req,res) =>{
+    res.writeHead(200,{'content-type':"text-plain"})
+    res.write("Hello world")
+    res.end();
+});
+    
 server.listen(3200,()=>{
     console.log('Application is running in port number 3200');
     
