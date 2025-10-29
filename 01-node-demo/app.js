eaa2da71598e7971752410caee01ecbb5ccc7df1
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-29 11:47:12 +0530

    STARTED WITH PACKAGE.JSON NODE WORKSPACE

--- 01-node-demo/app.js
+++ 01-node-demo/app.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+
+const http = require('http')
+
+let server = http.createServer();
+
+server.listen(3200,()=>{
+    console.log('Application is running in port number 3200');
+    
+})
