9c79b573e17fb0abbfc389202922549fd9d95e36
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-30 16:58:40 +0530

    COMPLETED MIDDLEWEAR LOGIN PAGE IN EXPRES NODE WORKSPACE

--- 01-node-workspace/app.js
+++ 01-node-workspace/app.js
@@ -82,6 +82,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.post('/login', (req,res) =>{
     console.log(req.body);
     res.send("Login sucessfull!")
